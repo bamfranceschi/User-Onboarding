afc17c8e3ee25a75fea8d316e7fa1bb8caef94fa
bamfranceschi <anna.franceschi@gmail.com>
2019-11-14 18:10:17 -0600

    finished mvp with stretch

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import FormikLOTRForm from "./components/Form";
+
+function App() {
+  return (
+    <div>
+      <h1>The Fellowship</h1>
+      <FormikLOTRForm />
+    </div>
+  );
+}
+
+export default App;
